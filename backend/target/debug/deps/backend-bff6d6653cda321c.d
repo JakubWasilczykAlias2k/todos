fc3ddac744fc06a9d0a75dd62b65d34fa7c22ac7
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2023-11-16 16:02:41 +0100

    Removed target from commit

--- backend/target/debug/deps/backend-bff6d6653cda321c.d
+++
@@ -1,5 +0,0 @@
-/Users/jakubwasilczyk/Sites/monorepos/todos_app/backend/target/debug/deps/backend-bff6d6653cda321c.rmeta: src/main.rs
-
-/Users/jakubwasilczyk/Sites/monorepos/todos_app/backend/target/debug/deps/backend-bff6d6653cda321c.d: src/main.rs
-
-src/main.rs:
--- backend/target/debug/deps/backend-f4d6c00adb23168e.d
+++
@@ -1,5 +0,0 @@
-/Users/jakubwasilczyk/Sites/monorepos/todos_app/backend/target/debug/deps/backend-f4d6c00adb23168e.rmeta: src/main.rs
-
-/Users/jakubwasilczyk/Sites/monorepos/todos_app/backend/target/debug/deps/backend-f4d6c00adb23168e.d: src/main.rs
-
-src/main.rs:
