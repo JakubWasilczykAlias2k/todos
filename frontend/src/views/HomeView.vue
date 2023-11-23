dc0aac10208dd9571c995ed1982adcd44c339cba
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2023-11-23 21:06:51 +0100

    Added frontend
    Code cleanup
    Started Homepage
    Created HeaderComponent

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+type ToDo = {
+  id: number
+  title: string
+  completed: boolean
+}
+const list: ToDo[] = [
+  {
+    id: 1,
+    title: 'Learn Vue 3',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'Learn TypeScript',
+    completed: false
+  },
+  {
+    id: 3,
+    title: 'Learn Tailwind CSS',
+    completed: true
+  }
+]
+</script>
+
+<template>
+  <main class="container text-red-500 mt-4">
+    <li v-for="item in list" :key="item.id">
+      {{ item.title }}
+    </li>
+  </main>
+</template>
