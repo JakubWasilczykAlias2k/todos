bcc9e9f863bf1afc5bbd1fef89e7695e0c702e00
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2023-11-30 16:30:16 +0100

    Created axum server for backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        .route("/stocazzo", get(root2));
+
+    // run our app with hyper
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:6969")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn root2() -> &'static str {
+    "Hello, Stocazzo!"
 }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 type ToDo = {
   id: number
   title: string
   completed: boolean
 }
-const list: ToDo[] = [
+const list = ref<ToDo[]>([
   {
     id: 1,
     title: 'Learn Vue 3',
@@ -20,11 +22,23 @@ const list: ToDo[] = [
     title: 'Learn Tailwind CSS',
     completed: true
   }
-]
+])
+const newItemTitle = ref('')
+function addOne() {
+  list.value.push({
+    id: list.value.length + 1,
+    title: newItemTitle.value,
+    completed: false
+  })
+}
 </script>
 
 <template>
-  <main class="container text-red-500 mt-4">
+  <main class="container mt-4">
+    <form class="my-4 flex gap-2" @submit.prevent="addOne">
+      <input id="title" name="title" type="text" v-model="newItemTitle" placeholder="Add item" />
+      <button class="bg-slate-500 rounded px-3 py-2 text-slate-50" type="submit">Add</button>
+    </form>
     <li v-for="item in list" :key="item.id">
       {{ item.title }}
     </li>
